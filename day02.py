77134b1bdf6fa371da277db2b970468bb51b2e92
Ashish <dhimanashish198@gmail.com>
2021-07-28 21:21:46 +0530

    first commit

---
+++ day02.py
@@ -0,0 +1,28 @@
+# # # # # for i in range(1,11):
+# # # # #     print(i,end=" ")
+
+# # # # l1= {1:"one",2:"two"}
+# # # # print(type(l1))
+# # # # for i in l1:
+# # # #     print(f'{i} {l1[i]}')
+
+# # # for i in "ASHISH":
+# # #     print(i,end="")
+# # # print("")
+
+# # # for i in range():
+# # #     print(i)
+
+# sum=0
+# print("enter the number for sum")
+# inp=int(input())
+# for i in range(inp+1):
+#     sum += i
+# # print(sum)
+
+n=0
+sum=0
+while(n<6):
+    sum+=n
+    n+=1
+print(sum)
---
+++ day1.py
@@ -0,0 +1,4 @@
+greeting="Hii!! "
+name="Ashish "
+end="Nice to meet you"
+print(greeting,name,end)
