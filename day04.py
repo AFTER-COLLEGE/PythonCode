6ac8814edfb82a6a5928227c4babc050f9336e76
Ashish <dhimanashish198@gmail.com>
2021-08-02 16:54:21 +0530

    updated

--- day04.py
+++ day04.py
@@ -1,21 +1,33 @@
 ### HACKERRANK PROBLEM on list ###
 
 
-n=int(input())
-k=[]
-for i in range(0,n):
-    cho=input().split()
-    if (cho[0]== "insert"):
-        k.insert(int(cho[1]),int(cho[2]))
-    elif (cho[0]== "print"):
-        print(k)
-    elif (cho[0]== "pop"):
-        k.pop(int(cho[1]))
-    elif (cho[0]== "append"):
-        k.append(int(cho[1]))
-    elif (cho[0]== "remove"):
-        k.remove(int(cho[1]))
-    elif (cho[0]== "sort"):
-        k.sort()
-    else:
-        k.reverse()
+# n=int(input())
+# k=[]
+# for i in range(0,n):
+#     cho=input().split()
+#     if (cho[0]== "insert"):
+#         k.insert(int(cho[1]),int(cho[2]))
+#     elif (cho[0]== "print"):
+#         print(k)
+#     elif (cho[0]== "pop"):
+#         k.pop(int(cho[1]))
+#     elif (cho[0]== "append"):
+#         k.append(int(cho[1]))
+#     elif (cho[0]== "remove"):
+#         k.remove(int(cho[1]))
+#     elif (cho[0]== "sort"):
+#         k.sort()
+#     else:
+#         k.reverse()
+
+##   MINI_MAX_SUM PROBLEM   ##
+arr=[1,2,3,4,5]
+arr.sort()
+minsum=0
+maxsum=0
+for i in range(4):
+    minsum+=arr[i]
+print(minsum,end=" ")
+for i in range(4,0,-1):
+    maxsum+=arr[i]
+print(maxsum)
--- day1.py
+++ day1.py
@@ -2,3 +2,8 @@ greeting="Hii!! "
 name="Ashish "
 end="Nice to meet you"
 print(greeting,name,end)
+l=[1,2,3]
+sum=0
+for i in l:
+    sum+=i
+print(sum)
