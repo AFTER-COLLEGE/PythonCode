a89a24c04012fb48ba110aeb44f5111e9d7c2e68
Ashish <dhimanashish198@gmail.com>
2021-08-09 21:11:09 +0530

    added new

---
+++ day07.py
@@ -0,0 +1,22 @@
+##     HACKER RANK      ##
+
+##_______SWAP CASE_______##
+# s=input()
+# ls=list(s)
+# for i in range(len(s)):
+#     if ls[i].isupper():
+#         ls[i]=ls[i].islower()
+#     else:
+#         ls[i]=ls[i].isupper()
+# t=""
+# for i in ls:
+#     t+=i
+# print(t)    
+
+n=int(input())
+for i in range(n):
+    ls=name,x,y,z=input().split(" ")
+    ls=list(ls)
+# result=input()
+# if result
+print(ls)
